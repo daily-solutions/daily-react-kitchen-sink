b62cf5f2aa2fab1e7a8d137b5380e1fc12109565
Dominic <dominic2401@gmail.com>
2024-07-20 18:04:13 +0900

    Updated code

--- src/rtcStats.js
+++ src/rtcStats.js
@@ -1,4 +1,4 @@
-import { getAllKeyNames, calculateSmoothness, filterPeriodMetrics, calculatePerSecondMetrics, updateCallTargets } from "./rtcStats/rtcStatsUtils.js";
+import { calculateSmoothness, filterPeriodMetrics, calculatePerSecondMetrics, updateCallTargets } from "./rtcStats/rtcStatsUtils.js";
 import { TARGETS, CUMULATIVE_KEYS, KINDS_TO_KEEP, KEYS_TO_KEEP, TYPES_TO_KEEP, SSRC_TO_REMOVE, KEYS_TO_KEEP_AFTER_PER_SECOND_CALCULATIONS } from './rtcStats/constants';
 
 export function rtcStats(config) {
@@ -16,131 +16,96 @@ export function rtcStats(config) {
     ...config
   };
 
-  // Reference all active WebRTC peer connections
   const peerConns = [];
-  const callAveragesArray = [];
+  const callAveragesArray = []; // Needs to be an array because of the peer connections. By the end of this process, we filter out empty ones. So in that case, there might be only one object in the array. But if we use this for other providers who use multiple peer connections, or track outbound metrics, this array will have more objects.
   const rawReports = {};
   const currentFilteredReports = {};
   const previousFilteredReports = {};
   const currentPerSecondReport = {};
-  const previousPerSecondReport = {};
 
-  function isObjectEmpty(obj) {
-    return Object.keys(obj).length === 0;
-  }
+  const isObjectEmpty = (obj) => Object.keys(obj).length === 0;
 
   async function store(data) {
     console.info("[RTCStats] Logging data...", data);
+    console.log("[RTCStats] Logging data at the time...", structuredClone(data));
     // Store report array here
   }
 
   async function logCallTarget(callTargetCollection) {
-
-    callTargetCollection.forEach(item => {
-      const callAverages = item.callAverages;
-      const callTargets = item.callTargets;
-
-      const ids = Object.keys(callAverages);
-
-      ids.forEach(id => {
+    callTargetCollection.forEach(({ callAverages, callTargets }) => {
+      Object.keys(callAverages).forEach(id => {
         updateCallTargets(callAverages, callTargets, TARGETS, id);
       });
     });
-
     const finalCallTargetCollection = {
+      reportNum: 0,
       test_id: _config.test_id,
       callTargetCollection
     };
+    finalCallTargetCollection.reportNum += 1;
 
     if (!isObjectEmpty(finalCallTargetCollection.callTargetCollection)) {
-      store(finalCallTargetCollection);
+      await store(finalCallTargetCollection);
     }
   }
-
 
   async function logBatch(batchCollection) {
-    // Note: async function so main event loop is kept unblocked
-    const reportArray = [];
-    batchCollection.forEach((b) => {
-      // grab the reports and empty the array (batch is cleared)
-      const reports = b.splice(0, b.length).forEach((data) =>
-        reportArray.push({
-          test_id: _config.test_id,
-          data,
-        })
-      );
-    });
+    const reportArray = batchCollection.flatMap(b =>
+      b.splice(0, b.length).map(data => ({
+        test_id: _config.test_id,
+        data,
+      }))
+    );
 
-    if (!reportArray.length) {
-      return;
+    if (reportArray.length) {
+      await store(reportArray);
     }
-    store(reportArray);
   }
 
-
-
-  /**
-   * -----------------------
-   * RTCPeerConnection shim
-   * -----------------------
-   */
   class RTCStatsPeerConnection extends RTCPeerConnection {
     constructor(config) {
       super(config);
 
-      // Init
       this.report = {
-        batch: [], // Array of reports collected
-        report_num: 0, // Current tick for timeseries
+        batch: [],
+        report_num: 0,
         connection_id: crypto.randomUUID(),
       };
       this.calculations = {
         smoothnessBuffer: [],
-        callAverages: {
-        },
-        callTargets: {
-        }
+        callAverages: {},
+        callTargets: {},
       };
 
-      // Append to global array
       peerConns.push(this.report);
-
-
-      callAveragesArray.push({ "callAverages": this.calculations.callAverages, "callTargets": this.calculations.callTargets });
-
-
+      callAveragesArray.push({ callAverages: this.calculations.callAverages, callTargets: this.calculations.callTargets });
 
       console.warn("PeerConnection instantiated", this.report);
 
-      // Listen for connection state, start harvesting when connected
-      this.addEventListener("connectionstatechange", () => {
-        clearInterval(this._statsInterval);
-
-        if (this.connectionState === "connected") {
-          this._getStats(this.getStats());
-
-          // Start collecting data every TICK...
-          this._statsInterval = setInterval(() => {
-            if (this.connectionState !== "connected")
-              return clearInterval(this._statsInterval);
-
-            // Run an override of the getStats method
-            this._getStats(this.getStats());
-          }, _config.report_interval * 1000);
-        }
-      });
+      this.addEventListener("connectionstatechange", this.handleConnectionStateChange.bind(this));
     }
 
+    handleConnectionStateChange() {
+      clearInterval(this._statsInterval);
 
+      if (this.connectionState === "connected") {
+        this._getStats(this.getStats());
+
+        this._statsInterval = setInterval(() => {
+          if (this.connectionState !== "connected") {
+            return clearInterval(this._statsInterval);
+          }
+          this._getStats(this.getStats());
+        }, _config.report_interval * 1000);
+      }
+    }
 
     async _getStats(getStatsPromise) {
       const stats = await getStatsPromise;
       const rtcdata = Object.fromEntries(stats.entries());
       if (!rtcdata) return;
 
-      // Here we store the previous filtered report period. This is used to calculate per second metrics.
       previousFilteredReports[this.report.connection_id] = { ...currentFilteredReports[this.report.connection_id] };
-      // This contains all of the metrics that we would expect from rtcStats
       rawReports[this.report.connection_id] = {
         clientId: _config.client_id,
         testId: _config.test_id,
@@ -148,57 +113,38 @@ export function rtcStats(config) {
         reportNum: this.report.report_num,
         ...rtcdata,
       };
-      // Filter data from the raw reports we're getting from rtcStats. We're only interested in certain types of metrics.
       currentFilteredReports[this.report.connection_id] = filterPeriodMetrics(rawReports[this.report.connection_id], TYPES_TO_KEEP, KINDS_TO_KEEP, SSRC_TO_REMOVE, KEYS_TO_KEEP);
-      // Then calculate the per second metrics based on the filtered reports. Report also contains targets.
-      // Here we grab the previous per second report period. This is used to calculate smoothness.
-      previousPerSecondReport[this.report.connection_id] = { ...currentPerSecondReport[this.report.connection_id] };
-      // Here we calculate the per second metrics based on the current and previous filtered reports.
       currentPerSecondReport[this.report.connection_id] = calculatePerSecondMetrics(currentFilteredReports[this.report.connection_id], previousFilteredReports[this.report.connection_id], CUMULATIVE_KEYS, KEYS_TO_KEEP_AFTER_PER_SECOND_CALCULATIONS, TARGETS, this.report.connection_id);
-      // If the per second reports are not empty, push them to the batch array. Reports may be empty because of peer connections that do not contain metrics we're interested in.
+
       if (!isObjectEmpty(currentPerSecondReport[this.report.connection_id])) {
-        let metricsWithSmoothness = {}
         this.calculations.smoothnessBuffer.push(currentPerSecondReport[this.report.connection_id]);
         if (this.calculations.smoothnessBuffer.length > _config.frameRatePeriodChange) {
           this.calculations.smoothnessBuffer.shift();
         }
-        // Here we add the smoothness metric to the per second report. It is currently based on the current per second metric, and the previous per second metric.
-        metricsWithSmoothness = calculateSmoothness(this.calculations.smoothnessBuffer, _config.frameRatePeriodChange, _config.frameRateDelta, this.calculations.callAverages);
-        //calculateSmoothness(currentPerSecondReport[this.report.connection_id], previousPerSecondReport[this.report.connection_id]);
+        const metricsWithSmoothness = calculateSmoothness(this.calculations.smoothnessBuffer, _config.frameRatePeriodChange, _config.frameRateDelta, this.calculations.callAverages);
         this.report.batch.push(metricsWithSmoothness);
-
         this.report.report_num += 1;
       }
     }
   }
 
-  /**
-   * -----------------------
-   * Init method
-   * -----------------------
-   */
   if (!["test_id", "client_id"].every((k) => k in _config)) {
     console.warn("[RTCStats] Missing config keys. Exiting");
   } else {
     console.info(`[RTCStats] Init with config:`, _config);
     RTCPeerConnection = RTCStatsPeerConnection;
 
-    // Main write interval
     setInterval(() => {
-      if (!peerConns.length) {
-        // No connected peers, do nothing
-        return;
-      }
-
-      // Create a batch of reports from each peer connection
-      const batchCollection = peerConns
-        .filter((pc) => pc.batch.length) // filter out PeerConnections with empty batches (no reports)
-        .map((pc) => pc.batch); // return the batch array containing all the reports (arbitrary amount)
-      const callTargetCollection = callAveragesArray
-        .filter((ca) => Object.keys(ca.callAverages).length)
-      if (batchCollection.length) {
-        logBatch(batchCollection);
-        logCallTarget(callTargetCollection);
+      if (peerConns.length) {
+        const batchCollection = peerConns
+          .filter((pc) => pc.batch.length)
+          .map((pc) => pc.batch);
+        const callTargetCollection = callAveragesArray
+          .filter((ca) => Object.keys(ca.callAverages).length);
+        if (batchCollection.length) {
+          logBatch(batchCollection);
+          logCallTarget(callTargetCollection);
+        }
       }
     }, _config.log_interval * 1000);
   }
--- src/rtcStats/rtcStatsUtils.js
+++ src/rtcStats/rtcStatsUtils.js
@@ -1,173 +1,98 @@
-/**
- * 
- * 
- * @param {*} currentPeriod 
- * @param {*} previousPeriod 
- * @param {*} CUMULATIVE_KEYS 
- * @param {*} KEYS_TO_KEEP_AFTER_PER_SECOND_CALCULATIONS 
- * @param {*} TARGETS 
- * @param {*} connection_id 
- * @returns 
- */
+export const calculatePerSecondMetrics = (currentPeriod, previousPeriod, CUMULATIVE_KEYS, KEYS_TO_KEEP_AFTER_PER_SECOND_CALCULATIONS, TARGETS, connection_id) => {
+    const perSecondMetrics = Object.entries(currentPeriod).reduce((acc, [key, current]) => {
+        const previous = previousPeriod[key];
+        let perSecondMetric = {};
 
+        if (previous) {
+            const deltaTime = (current.timestamp - previous.timestamp) / 1000;
 
-export function calculatePerSecondMetrics(currentPeriod, previousPeriod, CUMULATIVE_KEYS, KEYS_TO_KEEP_AFTER_PER_SECOND_CALCULATIONS, TARGETS, connection_id) {
-    const perSecondMetrics = {};
-
-    for (const key in currentPeriod) {
-        if (currentPeriod.hasOwnProperty(key)) {
-            const current = currentPeriod[key];
-            const previous = previousPeriod[key];
-            let perSecondMetric = {};
-
-            if (previous) {
-                const deltaTime = (current.timestamp - previous.timestamp) / 1000; // Convert milliseconds to seconds
-
-                if (deltaTime > 0) {
-                    CUMULATIVE_KEYS.forEach((metricKey) => {
-                        if (current[metricKey] !== undefined && previous[metricKey] !== undefined) {
-                            perSecondMetric[`${metricKey}PerSecond`] = (current[metricKey] - previous[metricKey]) / deltaTime;
-                        }
-                    });
-                }
-            } else {
+            if (deltaTime > 0) {
                 CUMULATIVE_KEYS.forEach((metricKey) => {
-                    if (current[metricKey] !== undefined) {
-                        perSecondMetric[`${metricKey}PerSecond`] = current[metricKey];
+                    if (current[metricKey] !== undefined && previous[metricKey] !== undefined) {
+                        perSecondMetric[`${metricKey}PerSecond`] = (current[metricKey] - previous[metricKey]) / deltaTime;
                     }
                 });
             }
-
-            perSecondMetrics[key] = {
-                'connection_id': connection_id,
-                ...removeKeysAfterPerSecondCalculations(current, KEYS_TO_KEEP_AFTER_PER_SECOND_CALCULATIONS),
-                ...perSecondMetric,
-            };
-
-        }
-    }
-
-    let perSecondMetricsWithTargets = calculatePerPeriodTargets(perSecondMetrics, TARGETS);
-    return perSecondMetricsWithTargets;
-}
-
-
-
-export function filterPeriodMetrics(currentPeriod, TYPES_TO_KEEP, KINDS_TO_KEEP, SSRC_TO_REMOVE, KEYS_TO_KEEP) {
-    const filteredMetrics = {};
-
-    for (const key in currentPeriod) {
-        const metric = currentPeriod[key];
-
-        // Check if the type is in TYPES_TO_KEEP
-        if (!TYPES_TO_KEEP.includes(metric.type)) {
-            continue;
+        } else {
+            CUMULATIVE_KEYS.forEach((metricKey) => {
+                if (current[metricKey] !== undefined) {
+                    perSecondMetric[`${metricKey}PerSecond`] = current[metricKey];
+                }
+            });
         }
 
-        // Check if the kind is in KINDS_TO_KEEP (if kind exists)
-        if (metric.kind && !KINDS_TO_KEEP.includes(metric.kind)) {
-            continue;
+        acc[key] = {
+            'connection_id': connection_id,
+            ...removeKeysAfterPerSecondCalculations(current, KEYS_TO_KEEP_AFTER_PER_SECOND_CALCULATIONS),
+            ...perSecondMetric,
+        };
+
+        return acc;
+    }, {});
+
+    return calculatePerPeriodTargets(perSecondMetrics, TARGETS);
+};
+
+export const filterPeriodMetrics = (currentPeriod, TYPES_TO_KEEP, KINDS_TO_KEEP, SSRC_TO_REMOVE, KEYS_TO_KEEP) =>
+    Object.entries(currentPeriod).reduce((acc, [key, metric]) => {
+        if (!TYPES_TO_KEEP.includes(metric.type) ||
+            (metric.kind && !KINDS_TO_KEEP.includes(metric.kind)) ||
+            (metric.ssrc && SSRC_TO_REMOVE.includes(metric.ssrc))) {
+            return acc;
         }
 
-        // Check if the ssrc is not in SSRC_TO_REMOVE (if ssrc exists)
-        if (metric.ssrc && SSRC_TO_REMOVE.includes(metric.ssrc)) {
-            continue;
-        }
-
-        // Filter the keys to only keep KEYS_TO_KEEP
-        const filteredMetric = {};
-        for (const keyToKeep of KEYS_TO_KEEP) {
+        acc[key] = KEYS_TO_KEEP.reduce((filteredMetric, keyToKeep) => {
             if (metric[keyToKeep] !== undefined) {
                 filteredMetric[keyToKeep] = metric[keyToKeep];
             }
-        }
+            return filteredMetric;
+        }, {});
 
-        filteredMetrics[key] = filteredMetric;
-    }
+        return acc;
+    }, {});
 
-    return filteredMetrics;
-}
-
-function removeKeysAfterPerSecondCalculations(metrics, KEYS_TO_KEEP_AFTER_PER_SECOND_CALCULATIONS) {
-    const filteredMetrics = {};
-
-    for (const keysToKeep of KEYS_TO_KEEP_AFTER_PER_SECOND_CALCULATIONS) {
-        if (metrics.hasOwnProperty(keysToKeep)) {
-            filteredMetrics[keysToKeep] = metrics[keysToKeep];
-        }
-    }
-    return filteredMetrics;
-}
-
-function calculatePerPeriodTargets(metrics, TARGETS) {
-    const targets = {};
-
-    for (const key in metrics) {
+const removeKeysAfterPerSecondCalculations = (metrics, KEYS_TO_KEEP_AFTER_PER_SECOND_CALCULATIONS) =>
+    KEYS_TO_KEEP_AFTER_PER_SECOND_CALCULATIONS.reduce((acc, key) => {
         if (metrics.hasOwnProperty(key)) {
-            const value = metrics[key];
-
-            for (const targetKey in TARGETS) {
-                if (TARGETS.hasOwnProperty(targetKey)) {
-                    const metricKey = targetKey.replace('Target', ''); // Remove 'Target' to match the metric key
-
-                    if (value.hasOwnProperty(metricKey)) {
-                        value[`${metricKey}TargetPct`] = (value[metricKey] / TARGETS[targetKey]) * 100;
-                    }
-                }
-            }
-
-            targets[key] = value;
+            acc[key] = metrics[key];
         }
-    }
+        return acc;
+    }, {});
 
-    return targets;
-}
-
-export function calculateSmoothness(smoothnessBuffer, frameRatePeriodChange, frameRateDelta, callAverages) {
-    let perSecondDataWithSmoothness = {};
-    let id = "";
-
-    // Initialize smoothness to 100 for all periods in the buffer
-    smoothnessBuffer.forEach(period => {
-        for (const key in period) {
-            if (period.hasOwnProperty(key)) {
-                period[key].smoothness = 100;
+const calculatePerPeriodTargets = (metrics, TARGETS) =>
+    Object.entries(metrics).reduce((acc, [key, value]) => {
+        acc[key] = Object.entries(TARGETS).reduce((targets, [targetKey, targetValue]) => {
+            const metricKey = targetKey.replace('Target', '');
+            if (value.hasOwnProperty(metricKey)) {
+                targets[`${metricKey}TargetPct`] = (value[metricKey] / targetValue) * 100;
             }
-        }
-    });
+            return targets;
+        }, value);
+        return acc;
+    }, {});
 
-    // Ensure the buffer has at least frameRatePeriodChange periods
+export const calculateSmoothness = (smoothnessBuffer, frameRatePeriodChange, frameRateDelta, callAverages) => {
     if (smoothnessBuffer.length < frameRatePeriodChange) {
-        return smoothnessBuffer[smoothnessBuffer.length - 1]; // Return the most recent period with smoothness set to 100
+        return smoothnessBuffer[smoothnessBuffer.length - 1];
     }
 
-    // Get the most recent period and the 5th period from the buffer
     const mostRecentPeriod = smoothnessBuffer[smoothnessBuffer.length - 1];
     const otherPeriod = smoothnessBuffer[smoothnessBuffer.length - frameRatePeriodChange];
 
-    for (const key in mostRecentPeriod) {
-        if (mostRecentPeriod.hasOwnProperty(key) && otherPeriod.hasOwnProperty(key)) {
-            const recentFramesDecoded = mostRecentPeriod[key].framesDecodedPerSecond;
-            const otherPeriodFramesDecoded = otherPeriod[key].framesDecodedPerSecond;
-
-            // We calculate smoothness by getting the framerate delta from the current period, and a period X periods ago
-            const frameRateDeltaMetric = Math.abs(recentFramesDecoded - otherPeriodFramesDecoded);
-
-            // If the delta is greater than the frameRateDelta specified, we say the stream is not smooth. Therefor 0%. If its less than the frameRateDelta, we say its 100% smooth.
+    return Object.entries(mostRecentPeriod).reduce((acc, [key, recentValue]) => {
+        if (otherPeriod.hasOwnProperty(key)) {
+            const frameRateDeltaMetric = Math.abs(recentValue.framesDecodedPerSecond - otherPeriod[key].framesDecodedPerSecond);
             const smoothness = frameRateDeltaMetric >= frameRateDelta ? 0 : 100;
+            const id = recentValue.id;
 
-            id = mostRecentPeriod[key].id;
-            console.log("+++++ id: ", id);
-
-            // Add smoothness metric to the most recent period
-            perSecondDataWithSmoothness[key] = {
-                ...mostRecentPeriod[key],
+            acc[key] = {
+                ...recentValue,
                 smoothness: smoothness,
             };
 
-            // Ensure callAverages has an entry for this id
             if (!callAverages[id]) {
                 callAverages[id] = {
+                    reportNum: 0,
                     frameHeight: { sum: 0, count: 0, avg: 0 },
                     framesDecodedPerSecond: { sum: 0, count: 0, avg: 0 },
                     freezeCountPerSecond: { sum: 0, count: 0, avg: 0 },
@@ -176,80 +101,29 @@ export function calculateSmoothness(smoothnessBuffer, frameRatePeriodChange, fra
                 };
             }
 
-            updateCallAverages(perSecondDataWithSmoothness[key], callAverages, id);
+            updateCallAverages(acc[key], callAverages, id);
         }
-    }
+        return acc;
+    }, {});
+};
 
+const updateCallAverages = (metrics, callAverages, id) => {
+    const metricsToUpdate = ['frameHeight', 'framesDecodedPerSecond', 'freezeCountPerSecond', 'totalFreezesDurationPerSecond', 'smoothness'];
+    callAverages[id].reportNum += 1;
 
-
-    return perSecondDataWithSmoothness;
-}
-
-function updateCallAverages(metrics, callAverages, id) {
-    // Ensure callAverages has an entry for this id
-    if (!callAverages[id]) {
-        callAverages[id] = {
-            frameHeight: { sum: 0, count: 0, avg: 0 },
-            framesDecodedPerSecond: { sum: 0, count: 0, avg: 0 },
-            freezeCountPerSecond: { sum: 0, count: 0, avg: 0 },
-            totalFreezesDurationPerSecond: { sum: 0, count: 0, avg: 0 },
-            smoothness: { sum: 0, count: 0, avg: 0 }
-        };
-    }
-
-    // Update frameHeight average
-    if (metrics.frameHeight !== undefined) {
-        callAverages[id].frameHeight.sum += metrics.frameHeight;
-        callAverages[id].frameHeight.count += 1;
-        callAverages[id].frameHeight.avg = callAverages[id].frameHeight.sum / callAverages[id].frameHeight.count;
-    }
-
-    // Update framesDecodedPerSecond average
-    if (metrics.framesDecodedPerSecond !== undefined) {
-        callAverages[id].framesDecodedPerSecond.sum += metrics.framesDecodedPerSecond;
-        callAverages[id].framesDecodedPerSecond.count += 1;
-        callAverages[id].framesDecodedPerSecond.avg = callAverages[id].framesDecodedPerSecond.sum / callAverages[id].framesDecodedPerSecond.count;
-    }
-
-    // Update freezeCountPerSecond average
-    if (metrics.freezeCountPerSecond !== undefined) {
-        callAverages[id].freezeCountPerSecond.sum += metrics.freezeCountPerSecond;
-        callAverages[id].freezeCountPerSecond.count += 1;
-        callAverages[id].freezeCountPerSecond.avg = callAverages[id].freezeCountPerSecond.sum / callAverages[id].freezeCountPerSecond.count;
-    }
-
-    // Update totalFreezesDurationPerSecond average
-    if (metrics.totalFreezesDurationPerSecond !== undefined) {
-        callAverages[id].totalFreezesDurationPerSecond.sum += metrics.totalFreezesDurationPerSecond;
-        callAverages[id].totalFreezesDurationPerSecond.count += 1;
-        callAverages[id].totalFreezesDurationPerSecond.avg = callAverages[id].totalFreezesDurationPerSecond.sum / callAverages[id].totalFreezesDurationPerSecond.count;
-    }
-
-    // Update smoothness average
-    if (metrics.smoothness !== undefined) {
-        callAverages[id].smoothness.sum += metrics.smoothness;
-        callAverages[id].smoothness.count += 1;
-        callAverages[id].smoothness.avg = callAverages[id].smoothness.sum / callAverages[id].smoothness.count;
-    }
-}
-
-export function getAllKeyNames(callTargetCollection) {
-    const keyNames = new Set(); // Use a set to avoid duplicate keys
-
-    callTargetCollection.forEach(item => {
-        if (item.callAverages) {
-            Object.keys(item.callAverages).forEach(key => {
-                keyNames.add(key);
-            });
+    metricsToUpdate.forEach(metric => {
+        if (metrics[metric] !== undefined) {
+            callAverages[id][metric].sum += metrics[metric];
+            callAverages[id][metric].count += 1;
+            callAverages[id][metric].avg = callAverages[id][metric].sum / callAverages[id][metric].count;
         }
     });
+};
 
-    return Array.from(keyNames);
-}
-
-export function updateCallTargets(callAverages, callTargets, TARGETS, id) {
+export const updateCallTargets = (callAverages, callTargets, TARGETS, id) => {
     if (!callTargets[id]) {
         callTargets[id] = {
+            reportNum: 0,
             frameHeightTargetPct: 100,
             framesDecodedPerSecondTargetPct: 100,
             freezeCountPerSecondTargetPct: 100,
@@ -258,25 +132,19 @@ export function updateCallTargets(callAverages, callTargets, TARGETS, id) {
         };
     }
 
-    // Update frameHeight target
-    if (callAverages[id].frameHeight.avg !== undefined) {
-        callTargets[id].frameHeightTargetPct = (callAverages[id].frameHeight.avg / TARGETS.frameHeight) * 100;
-    }
-    // Update framesDecodedPerSecond target
-    if (callAverages[id].framesDecodedPerSecond.avg !== undefined) {
-        callTargets[id].framesDecodedPerSecondTargetPct = (callAverages[id].framesDecodedPerSecond.avg / TARGETS.framesDecodedPerSecond) * 100;
-    }
-    // Update freezeCountPerSecond target
-    if (callAverages[id].freezeCountPerSecond.avg !== undefined) {
-        callTargets[id].freezeCountPerSecondTargetPct = ((1 - callAverages[id].freezeCountPerSecond.avg) / TARGETS.freezeCountPerSecond) * 100;
-    }
-    // Update totalFreezesDurationPerSecond target
-    if (callAverages[id].totalFreezesDurationPerSecond.avg !== undefined) {
-        callTargets[id].totalFreezesDurationPerSecondTargetPct = ((1 - callAverages[id].totalFreezesDurationPerSecond.avg) / TARGETS.totalFreezesDurationPerSecond) * 100;
-    }
-    // Update smoothness target
-    if (callAverages[id].smoothness.avg !== undefined) {
-        callTargets[id].smoothnessTargetPct = (callAverages[id].smoothness.avg / TARGETS.smoothnessTarget) * 100;
-    }
+    const metricsToUpdate = [
+        { metric: 'frameHeight', target: 'frameHeight' },
+        { metric: 'framesDecodedPerSecond', target: 'framesDecodedPerSecond' },
+        { metric: 'freezeCountPerSecond', target: 'freezeCountPerSecond', invert: true },
+        { metric: 'totalFreezesDurationPerSecond', target: 'totalFreezesDurationPerSecond', invert: true },
+        { metric: 'smoothness', target: 'smoothnessTarget' }
+    ];
 
-}
+    metricsToUpdate.forEach(({ metric, target, invert }) => {
+        if (callAverages[id][metric].avg !== undefined) {
+            const value = invert ? 1 - callAverages[id][metric].avg : callAverages[id][metric].avg;
+            callTargets[id][`${metric}TargetPct`] = (value / TARGETS[target]) * 100;
+            callTargets[id].reportNum = callAverages[id].reportNum;
+        }
+    });
+};
