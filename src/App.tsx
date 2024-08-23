17177bf5065d9e1ec3a7e14614e3a04524a0fc37
James Hush <james@daily.co>
2024-08-23 14:02:25 +0800

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ export default function App() {
 
   const [enableBlurClicked, setEnableBlurClicked] = useState(false);
   const [enableBackgroundClicked, setEnableBackgroundClicked] = useState(false);
-  const [dailyRoomUrl, setDailyRoomUrl] = useState("https://hush.daily.co/sfu");
+  const [dailyRoomUrl, setDailyRoomUrl] = useState("");
   const [dailyMeetingToken, setDailyMeetingToken] = useState("");
 
   const {
