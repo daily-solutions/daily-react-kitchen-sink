ad1e524f273358bd437b9ec551bdd42df8c5bd3a
James Hush <james@daily.co>
2024-09-12 15:27:35 +0800

    Open Prebuilt in a pop up window

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Prebuilt } from "./Prebuilt";
-import { DailyProvider } from "@daily-co/daily-react";
-import App from "./App";
+import { NewWindow } from "./newWindow";
 
 const container = document.getElementById("root");
 
@@ -17,16 +16,5 @@ const urlParams = new URLSearchParams(window.location.search);
 const isPrebuilt = urlParams.get("prebuilt") ?? false;
 
 root.render(
-  <StrictMode>
-    {isPrebuilt ? (
-      <Prebuilt />
-    ) : (
-      <DailyProvider
-        subscribeToTracksAutomatically={false}
-        dailyConfig={{ useDevicePreferenceCookies: true }}
-      >
-        <App />
-      </DailyProvider>
-    )}
-  </StrictMode>
+  <StrictMode>{isPrebuilt ? <Prebuilt /> : <NewWindow />}</StrictMode>
 );
