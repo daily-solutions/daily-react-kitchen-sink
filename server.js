ad7748d0a1121bada3092ace1a4ad6d63265b2ce
James Hush <james@daily.co>
2024-09-25 15:04:19 +0800

    It works

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ if (!DAILY_API_KEY) {
 
 const app = express();
 
-// EXAMPLE appending a customerId to every room created
+app.use(express.json());
+
 app.post("/rooms", async (request, response) => {
   const roomProperties = { ...request.body };
 
@@ -23,6 +24,28 @@ app.post("/rooms", async (request, response) => {
       Authorization: `Bearer ${DAILY_API_KEY}`,
     },
     body: JSON.stringify(roomProperties),
+  });
+
+  // Parse the response from Daily API
+  const dailyRoom = await dailyResponse.json();
+
+  // Send the response back to the client
+
+  response.json(dailyRoom);
+});
+
+app.post("/meeting-tokens", async (request, response) => {
+  console.log("request.body", request.body);
+  const tokenProperties = { ...request.body };
+
+  // Use fetch to send a POST request to Daily API to create a meeting token
+  const dailyResponse = await fetch("https://api.daily.co/v1/meeting-tokens", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${DAILY_API_KEY}`,
+    },
+    body: JSON.stringify(tokenProperties),
   });
 
   // Parse the response from Daily API
